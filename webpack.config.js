f4ced4960e951247aa2691446c1398b769355c30
Fabio Biondi <info@fabiobiondi.com>
2020-02-24 03:52:12 +0100

    Add support for JS files

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.ts',
+    app: './src/index.js',
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -32,7 +32,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.tsx?$/,
+        test: /\.(ts|js)x?$/,
         use: [
           'ts-loader',
         ],
