feb2720244d17f8b142d537a34acd8a7763a3cdf
Fabio Biondi <info@fabiobiondi.com>
2020-02-24 12:41:57 +0100

    Switch to TS

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
+    app: './src/index.ts',
   },
   devtool: 'inline-source-map',
   devServer: {
